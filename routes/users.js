167b5650c633bfce62fd4d0faf7aa56a8d285fa6
haywhyzero <90219922+haywhyzero@users.noreply.github.com>
2022-11-02 10:09:28 +0100

    my first api

---
+++ routes/users.js
@@ -0,0 +1,24 @@
+import express  from "express";
+import { updateUser, deleteUser, getUser, getUsers } from './../controllers/user.js';
+import { verifyToken, verifyUser,verifyAdmin } from "../utils/verifyToken.js";
+
+const router = express.Router();
+
+router.get("/checkauthentication", verifyToken, (req,res,next)=>{
+    res.send("hello user,you are logged in")
+})
+router.get("/checkuser/:id", verifyUser, (req,res,next)=>{
+    res.send("hello user,you are logged in and you can delete your account")
+})
+router.get("/checkadmin/:id", verifyAdmin, (req,res,next)=>{
+    res.send("hello admin,you have the whole world in your hands")
+})
+// UPDATE
+router.put("/:id", verifyUser,updateUser)
+// DELETE
+router.delete("/:id", verifyUser,deleteUser)
+// GET
+router.get("/:id", verifyUser,getUser)  
+// GET ALL
+router.get("/", verifyAdmin,getUsers)
+export default router
